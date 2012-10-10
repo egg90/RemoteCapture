27509cb369c7aec01298d4a3c8dfe349eb89a358
eggfly <lihaohua90@gmail.com>
2012-10-10 15:16:25 +0800

    diff well

---
+++ RemoteCapture/Common/BitmapCompare.cs
@@ -0,0 +1,110 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BitmapCompare.cs" company="eggfly">
+//     Copyright (c) eggfly. All rights reserved.
+// </copyright>
+// <author>eggfly</author>
+//-----------------------------------------------------------------------
+
+namespace RemoteCapture.Common
+{
+    using System;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// BitmapCompare Class
+    /// </summary>
+    public class BitmapCompare
+    {
+        /// <summary>
+        /// Compares the specified Y buffer A.
+        /// </summary>
+        /// <param name="bufferA">The buffer A.</param>
+        /// <param name="bufferB">The buffer B.</param>
+        /// <returns>
+        /// The match precent.
+        /// </returns>
+        public static double Compare(byte[] bufferA, byte[] bufferB)
+        {
+            bool different = false;
+            for (int i = 0; i < bufferA.Length; i++)
+            {
+                if (bufferA[i] != bufferB[i])
+                {
+                    different = true;
+                    break;
+                }
+            }
+
+            different = !different;
+
+            var hisogramA = GetHisogram(bufferA);
+            var hisogramB = GetHisogram(bufferB);
+            return CompareHisogram(hisogramA, hisogramB);
+        }
+
+        /// <summary>
+        /// Compares the hisogram.
+        /// </summary>
+        /// <param name="hisogramA">The hisogram A.</param>
+        /// <param name="hisogramB">The hisogram B.</param>
+        /// <returns>The match precent.</returns>
+        private static double CompareHisogram(int[] hisogramA, int[] hisogramB)
+        {
+            if (hisogramA.Length != hisogramB.Length)
+            {
+                return 0.0;
+            }
+
+            int len = hisogramA.Length;
+            double result = 0;
+            for (int i = 0; i < len; i++)
+            {
+                double match;
+                int a = hisogramA[i];
+                int b = hisogramB[i];
+                if (a != b)
+                {
+                    int ddd = 0;
+                    ddd++;
+                }
+
+                if (a == 0 && b == 0)
+                {
+                    match = 1.0;
+                }
+                else
+                {
+                    match = 1.0 - ((double)Math.Abs(a - b) / Math.Max(a, b));
+                }
+
+                result += match;
+            }
+
+            return result / len;
+        }
+
+        /// <summary>
+        /// Gets the hisogram.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns>The hisogram.</returns>
+        private static int[] GetHisogram(byte[] buffer)
+        {
+            int[] hisogram = new int[byte.MaxValue + 1];
+            foreach (byte b in buffer)
+            {
+                hisogram[b]++;
+            }
+
+            return hisogram;
+        }
+    }
+}
--- RemoteCapture/Views/MainPage.xaml.cs
+++ RemoteCapture/Views/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace RemoteCapture.Views
     using System.Collections.Generic;
     using System.Linq;
     using System.Net;
+    using System.Threading;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Documents;
@@ -20,12 +21,20 @@ namespace RemoteCapture.Views
     using System.Windows.Shapes;
     using Microsoft.Devices;
     using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+    using Microsoft.Xna.Framework.GamerServices;
+    using RemoteCapture.Common;
 
     /// <summary>
     /// Main Page
     /// </summary>
     public partial class MainPage : PhoneApplicationPage
     {
+        /// <summary>
+        /// Different Precent
+        /// </summary>
+        private const double CompareDifferentPrecent = 0.85;
+
         /// <summary>
         /// Photo Camera
         /// </summary>
@@ -35,6 +44,26 @@ namespace RemoteCapture.Views
         /// Capture Source
         /// </summary>
         private CaptureSource captureSource = new CaptureSource();
+
+        /// <summary>
+        /// two buffer
+        /// </summary>
+        private byte[] buffer1, buffer2;
+
+        /// <summary>
+        /// current buffer
+        /// </summary>
+        private byte[] currentBuffer;
+        
+        /// <summary>
+        /// Pump Preview Frames
+        /// </summary>
+        private bool pumpPreviewFrames = false;
+
+        /// <summary>
+        /// pump thread
+        /// </summary>
+        private Thread pumpThread;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MainPage"/> class.
@@ -65,16 +94,6 @@ namespace RemoteCapture.Views
         }
 
         /// <summary>
-        /// Handles the Click event of the button1 control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            this.camera.CaptureImage();
-        }
-
-        /// <summary>
         /// Called when [camera capture image available].
         /// </summary>
         /// <param name="sender">The sender.</param>
@@ -102,6 +121,122 @@ namespace RemoteCapture.Views
         private void OnCameraInitialized(object sender, CameraOperationCompletedEventArgs e)
         {
             this.camera.FlashMode = FlashMode.Off;
+        }
+
+        /// <summary>
+        /// Pumps the preview frames.
+        /// </summary>
+        private void PumpPreviewFrames()
+        {
+            this.buffer1 = this.buffer1 ?? new byte[(int)this.camera.PreviewResolution.Width * (int)this.camera.PreviewResolution.Height];
+            this.buffer2 = this.buffer2 ?? new byte[(int)this.camera.PreviewResolution.Width * (int)this.camera.PreviewResolution.Height];
+            this.currentBuffer = this.buffer1;
+
+            try
+            {
+                while (this.pumpPreviewFrames)
+                {
+                    this.camera.GetPreviewBufferY(this.currentBuffer);
+                    double result = BitmapCompare.Compare(this.buffer1, this.buffer2);
+
+                    this.Dispatcher.BeginInvoke(() =>
+                    {
+                        this.textBlock1.Text = result.ToString();
+                        if (result < CompareDifferentPrecent)
+                        {
+                            this.ellipse1.Fill = new SolidColorBrush(Colors.Red);
+                        }
+                        else
+                        {
+                            this.ellipse1.Fill = new SolidColorBrush(Colors.Green);
+                        }
+                    });
+
+                    this.currentBuffer = (this.currentBuffer == this.buffer1) ? this.buffer2 : this.buffer1;
+
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception e)
+            {
+                this.Dispatcher.BeginInvoke(() =>
+                {
+                    this.textBlock1.Text = e.Message;
+                });
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the button1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Button1.IsEnabled = false;
+            ////this.camera.CaptureImage();
+            if (this.buffer1 == null)
+            {
+                this.buffer1 = new byte[(int)this.camera.PreviewResolution.Width * (int)this.camera.PreviewResolution.Height];
+            }
+
+            this.camera.GetPreviewBufferY(this.buffer1);
+            this.Button1.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Button2 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Button2.IsEnabled = false;
+            if (this.buffer2 == null)
+            {
+                this.buffer2 = new byte[(int)this.camera.PreviewResolution.Width * (int)this.camera.PreviewResolution.Height];
+            }
+
+            this.camera.GetPreviewBufferY(this.buffer2);
+            this.Button2.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Button3 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.buffer1 != null && this.buffer2 != null)
+            {
+                double result = BitmapCompare.Compare(this.buffer1, this.buffer2);
+                this.textBlock1.Text = result.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Button4 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            this.pumpPreviewFrames = true;
+            this.pumpThread = new Thread(this.PumpPreviewFrames);
+            this.pumpThread.Start();
+            Guide.IsScreenSaverEnabled = false;
+            PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Enabled;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Button5 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            this.pumpPreviewFrames = false;
         }
     }
 }
